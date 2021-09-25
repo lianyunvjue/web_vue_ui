55edb89b22556ddf4df0ce29f78a9351bd349bf8
Nicky <heishaluo@163.com>
2021-09-25 19:29:31 +0800

    http

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,13 +1,32 @@
 <template>
-  <h3>{{ name }}</h3>
+  <div>
+    <h3>{{ name }}</h3>
+    <div v-for="post in posts" :key="post.id">
+      {{ post.title }}-
+      <small> {{ post.user.name }} </small>
+    </div>
+  </div>
 </template>
 
 <script>
+import { axios } from '@/app/app.service';
+
 export default {
   data() {
     return {
       name: '宁皓网',
+      posts: [],
+      errorMessage: '',
     };
+  },
+
+  async created() {
+    try {
+      const response = await axios.get('/posts');
+      this.posts = response.data;
+    } catch (error) {
+      this.errorMessage = error.message;
+    }
   },
 };
 </script>
